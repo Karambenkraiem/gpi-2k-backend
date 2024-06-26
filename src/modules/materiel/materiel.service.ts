0b23e807cefb9da1613990706f5fad2771a9aab2
Khmhamdi <kh.mhamdi@gmail.com>
2024-06-26 19:33:46 +0100

    Affectation

--- src/modules/materiel/materiel.service.ts
+++ src/modules/materiel/materiel.service.ts
@@ -18,47 +18,18 @@ export class MaterielService {
 
   findAll() {
     return this.prisma.materiel.findMany({
-<<<<<<< HEAD
       include: {Affectation: true,Emprunt:true}
       });
   }
 
 
-=======
-      include: {
-        Affectation: {
-          select: {
-            etatAffectation: true,
-          },
-        },
-        Emprunt: {
-          select: {
-            etatEmprunt: true,
-          },
-        },
-      },
-    });
-  }  
-  // findAll() {
-  //   return this.prisma.materiel.findMany({
-  //     include: {
-  //       Affectation: true,
-  //       Emprunt: true
-  //     }
-  //   })
-  // }
->>>>>>> 3ab1ce8c2296cfb6b39f219be2a6e50f84519c9d
   findOne(numeroSerie: string) {
     return this.prisma.materiel.findUnique({
       where: { numeroSerie },
     });
-<<<<<<< HEAD
   }
 
   
-=======
-  } 
->>>>>>> 3ab1ce8c2296cfb6b39f219be2a6e50f84519c9d
 
   update(numeroSerie: string, updateMaterielDto: UpdateMaterielDto) {
     const { dateAcquisition, ...rest } = updateMaterielDto;
